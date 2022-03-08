0477eb832ccba8688bceec471a04a96c26be550c
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-03-08 09:21:58 +0100

    opdracht 1 gemaakt

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,9 @@
 
+-- Studenten Insert --
+
+INSERT INTO `studenten` (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (1, 'Henk', 'van Balen', '9A', 'Albert Cuypstraat 1', '1234AB', '2000-05-18', 'Amsterdam');
+
+-- PRECIES ZO GEDAAN VOOR DE REST VAN DE STUDENTEN, VANDAAR DAT IK DE CODES NIET ALLEMAAL 1 VOOR 1 ER IN HEB GEGOOIT --
+
+
