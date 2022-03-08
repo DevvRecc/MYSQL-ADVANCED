beb6b087f92772625ddb2f26a51e7744d672d30b
Recep Yildiz <recep.yildiz@student.rocva.nl>
2022-03-08 09:43:36 +0100

    1.2 gemaakt

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,14 @@
+-- Hier voeg ik de opdraachten --
 
+INSERT INTO studenten (student_id, voornaam, achternaam, woonplaats)
+VALUES (8, 'Jan', 'Vermeer', 'Amsterdam');
+
+--                                                                --
+
+INSERT INTO studenten (student_id, voornaam, achternaam, woonplaats)
+VALUES (9, 'Bert', 'Hendriks', 'Utrecht');
+
+--                                                                --
+
+INSERT INTO studenten (student_id, voornaam, achternaam, woonplaats)
+VALUES (10, 'Mari', 'Janssen', 'Almere');
